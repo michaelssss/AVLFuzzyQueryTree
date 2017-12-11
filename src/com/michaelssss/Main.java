84f006d73511d24151504ad56616cf801e522267
Michaelssss <liangyuming2@gmail.com>
2017-12-11 21:57:49 +0800

    性能测试

--- src/com/michaelssss/Main.java
+++ src/com/michaelssss/Main.java
@@ -6,46 +6,53 @@ public class Main {
         FuzzyLookUpContainer fuzzyLookUpContainer = new FuzzyLookUpBinarySearchTree();
         FuzzyLookUpContainer fuzzyLookUpContainer1 = new FuzzyLookUpBinaryTree();
         FuzzyLookUpContainer fuzzyLookUpContainer2 = new FuzzyLookUpList();
-        int round = 50000;
+        int round = 100000;
         long begin = System.currentTimeMillis();
         for (int i = 0; i < round; i++) {
             TestObject testObject = new TestObject(Integer.toString(i));
             fuzzyLookUpContainer.put(testObject);
         }
-        System.out.println("build avl tree cost time: " + Long.toString(System.currentTimeMillis() - begin));
+        System.out.println("build avl tree cost time: " + Long.toString(System.currentTimeMillis() - begin) + " ms");
+        begin = System.currentTimeMillis();
         for (int i = 0; i < round; i++) {
             TestObject testObject = new TestObject(Integer.toString(i));
             fuzzyLookUpContainer1.put(testObject);
         }
-        System.out.println("build binary tree cost time: " + Long.toString(System.currentTimeMillis() - begin));
+        System.out.println("build binary tree cost time: " + Long.toString(System.currentTimeMillis() - begin) + " ms");
+        begin = System.currentTimeMillis();
         for (int i = 0; i < round; i++) {
             TestObject testObject = new TestObject(Integer.toString(i));
             fuzzyLookUpContainer2.put(testObject);
         }
-        System.out.println("build FuzzyLookUpList cost time: " + Long.toString(System.currentTimeMillis() - begin));
+        System.out.println("build FuzzyLookUpList cost time: " + Long.toString(System.currentTimeMillis() - begin) + " ms");
         begin = System.currentTimeMillis();
         for (int i = 0; i < round; i++) {
             fuzzyLookUpContainer.lookUp(new TestObject(Integer.toString(i)));
         }
-        System.out.println("avl queryTime:" + Long.toString(System.currentTimeMillis() - begin));
+        System.out.println("avl queryTime:" + Long.toString(System.currentTimeMillis() - begin) + " ms");
         begin = System.currentTimeMillis();
         for (int i = 0; i < round; i++) {
             fuzzyLookUpContainer1.lookUp(new TestObject(Integer.toString(i)));
         }
-        System.out.println("binary queryTime:" + Long.toString(System.currentTimeMillis() - begin));
+        System.out.println("binary queryTime:" + Long.toString(System.currentTimeMillis() - begin) + " ms");
         begin = System.currentTimeMillis();
         for (int i = 0; i < round; i++) {
             fuzzyLookUpContainer2.lookUp(new TestObject(Integer.toString(i)));
         }
-        System.out.println("FuzzyLookUpList queryTime:" + Long.toString(System.currentTimeMillis() - begin));
-//        SimpleMap<String, TestObject> map = new SimpleMap<>(64, 0.001f);
+        System.out.println("FuzzyLookUpList queryTime:" + Long.toString(System.currentTimeMillis() - begin) + " ms");
+        SimpleMap<Integer, TestObject> map = new SimpleMap<>(64);
+        begin = System.currentTimeMillis();
+        for (int i = 0; i < round; i++) {
+            TestObject testObject = new TestObject(Integer.toString(i));
+            map.put(i, testObject);
+        }
+        System.out.println("SimpleMap insertTime:" + Long.toString(System.currentTimeMillis() - begin) + " ms");
+        begin = System.currentTimeMillis();
+        for (int i = 0; i < round; i++) {
+            map.get(i);
+        }
+        System.out.println("SimpleMap queryTime:" + Long.toString(System.currentTimeMillis() - begin) + " ms");
 
-//        map.put("1", testObject);
-//        map.put("2", testObject1);
-//        map.put("3", testObject2);
-//        map.put("4", testObject3);
-//        map.put("5", testObject4);
-//        map.put("6", testObject5);
 //        System.out.println(map.get("1").equals(testObject));
 //        System.out.println(map.get("2").equals(testObject1));
 //        System.out.println(map.get("3").equals(testObject2));
